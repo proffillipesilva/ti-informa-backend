5b09bbff79f78481e33ee353953d5ebf2f8e0a52
LeoYujiFIEC <leonardo37647@fiec.edu.br>
2025-05-07 00:17:38 -0300

    Alterado

--- src/main/java/br/com/tiinforma/backend/domain/usuario/Usuario.java
+++ src/main/java/br/com/tiinforma/backend/domain/usuario/Usuario.java
@@ -25,7 +25,7 @@ public class Usuario implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_usuario;
+    private Long id;
 
     private String nome;
 
