481469228f51c8067d44fd58b515b6d05d3c8cf0
SamuelRaymundo <161658735+SamuelRaymundo@users.noreply.github.com>
2025-05-14 22:00:25 -0300

    Adicionando HATEOAS ao Criador

--- src/main/java/br/com/tiinforma/backend/controller/CriadorController.java
+++ src/main/java/br/com/tiinforma/backend/controller/CriadorController.java
@@ -1,10 +1,15 @@
 package br.com.tiinforma.backend.controller;
 
 import br.com.tiinforma.backend.domain.criador.CriadorCreateDto;
+import br.com.tiinforma.backend.domain.criador.CriadorModel;
+import br.com.tiinforma.backend.domain.criador.CriadorModelAssembler;
 import br.com.tiinforma.backend.domain.criador.CriadorResponseDto;
+import br.com.tiinforma.backend.domain.usuario.UsuarioModel;
+import br.com.tiinforma.backend.domain.usuario.UsuarioModelAssembler;
 import br.com.tiinforma.backend.services.interfaces.CriadorService;
 import br.com.tiinforma.backend.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,29 +23,45 @@ public class CriadorController {
     @Autowired
     private CriadorService criadorService;
 
+    @Autowired
+    private CriadorModelAssembler criadorModel;
+
+
+
     @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, "application/x-yml"})
-    public ResponseEntity<CriadorResponseDto> findById(@PathVariable Long id) {
-        return ResponseEntity.ok(criadorService.findById(id));
+    public ResponseEntity<CriadorModel> findById(@PathVariable Long id) {
+        CriadorResponseDto criadorResponseDto = criadorService.findById(id);
+        CriadorModel model = criadorModel.toModel(criadorResponseDto);
+        return ResponseEntity.ok(model);
     }
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, "application/x-yml"})
-    public ResponseEntity<List<CriadorResponseDto>> findAll() {
-        return ResponseEntity.ok(criadorService.findAll());
+    public ResponseEntity<CollectionModel<CriadorModel>> findAll() {
+       List<CriadorResponseDto> criadorResponseDto = criadorService.findAll();
+       List<CriadorModel> model = criadorResponseDto.stream()
+               .map(criadorModel::toModel)
+               .toList();
+       return ResponseEntity.ok(CollectionModel.of(model));
+
     }
 
     @PostMapping(value = "/register", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, "application/x-yml"}, consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, "application/x-yml"})
-    public CriadorResponseDto create(@RequestBody CriadorCreateDto criadorCreateDto) {
-        return criadorService.create(criadorCreateDto);
+    public ResponseEntity<CriadorModel> create(@RequestBody CriadorCreateDto criadorCreateDto) {
+        CriadorResponseDto criadorResponseDto = criadorService.create(criadorCreateDto);
+        CriadorModel model = criadorModel.toModel(criadorResponseDto);
+        return ResponseEntity.ok(model);
     }
 
     @PutMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, "application/x-yml"}, consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, "application/x-yml"})
-    public ResponseEntity<CriadorResponseDto> update(@PathVariable Long id, @RequestBody CriadorCreateDto dto) {
+    public ResponseEntity<CriadorModel> update(@PathVariable Long id, @RequestBody CriadorCreateDto dto) {
         CriadorResponseDto criadorAtualizado = criadorService.update(id, dto);
-        return ResponseEntity.ok(criadorAtualizado);
+        CriadorModel model = criadorModel.toModel(criadorAtualizado);
+        return ResponseEntity.ok(model);
     }
 
     @DeleteMapping(value = "/{id}")
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         criadorService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
