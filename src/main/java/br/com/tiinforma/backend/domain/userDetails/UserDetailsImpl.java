b980ab836b70124462ccf4d288c92b2ae6754318
LeoYujiFIEC <leonardo37647@fiec.edu.br>
2025-05-18 13:19:10 -0300

    Aprimorado Upload de videos e playlists

--- src/main/java/br/com/tiinforma/backend/domain/userDetails/UserDetailsImpl.java
+++ src/main/java/br/com/tiinforma/backend/domain/userDetails/UserDetailsImpl.java
@@ -1,52 +1,33 @@
 package br.com.tiinforma.backend.domain.userDetails;
 
-
-import br.com.tiinforma.backend.domain.criador.Criador;
 import br.com.tiinforma.backend.domain.enums.Funcao;
-import br.com.tiinforma.backend.domain.usuario.Usuario;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
 import java.util.Collection;
 import java.util.Collections;
-
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class UserDetailsImpl implements UserDetails {
 
     private static final long serialVersionUID = 1L;
 
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String senha;
     private Funcao funcao;
 
-
-    public UserDetailsImpl(Usuario usuario) {
-        this.id = usuario.getId_usuario();
-        this.email = usuario.getEmail();
-        this.senha = usuario.getSenha();
-        this.funcao = usuario.getFuncao(); // ROLE_USER
-    }
-
-    public UserDetailsImpl(Criador criador) {
-        this.id = criador.getId_criador();
-        this.email = criador.getEmail();
-        this.senha = criador.getSenha();
-        this.funcao = criador.getFuncao(); // ROLE_CRIADOR
+    public UserDetailsImpl(Long id, String email, String senha, Funcao funcao) {
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
+        this.funcao = funcao;
     }
 
     @Override
@@ -83,5 +64,4 @@ public class UserDetailsImpl implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
-
 }
