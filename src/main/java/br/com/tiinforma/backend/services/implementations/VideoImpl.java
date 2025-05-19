21602aceb9692dc9301483515b68e32564134b41
Maria <maria37411@fiec.edu.br>
2025-05-19 19:23:09 -0300

    Ajustando VideoImpl

--- src/main/java/br/com/tiinforma/backend/services/implementations/VideoImpl.java
+++ src/main/java/br/com/tiinforma/backend/services/implementations/VideoImpl.java
@@ -3,13 +3,11 @@ package br.com.tiinforma.backend.services.implementations;
 import br.com.tiinforma.backend.domain.video.Video;
 import br.com.tiinforma.backend.repositories.VideoRepository;
 import br.com.tiinforma.backend.services.interfaces.VideoService;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@AllArgsConstructor
 @Service
 public class VideoImpl implements VideoService {
 
