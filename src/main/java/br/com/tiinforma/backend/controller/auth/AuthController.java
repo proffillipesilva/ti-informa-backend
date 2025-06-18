223b670a9cabd5cbaf639e4e47513d25d458efec
LeoYujiFIEC <leonardo37647@fiec.edu.br>
2025-06-17 23:46:26 -0300

    Adicionado endpoints

--- src/main/java/br/com/tiinforma/backend/controller/auth/AuthController.java
+++ src/main/java/br/com/tiinforma/backend/controller/auth/AuthController.java
@@ -38,7 +38,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
@@ -277,42 +280,9 @@ public class AuthController {
             Optional<Criador> criadorOptional = criadorRepository.findByEmail(usuario.getEmail());
             if (criadorOptional.isPresent()) {
                 Criador criador = criadorOptional.get();
-                return ResponseEntity.ok(Map.of(
-                        "id", usuario.getId(),
-                        "nome", criador.getNome(),
-                        "email", criador.getEmail(),
-                        "isCriador", true,
-                        "funcao", criador.getFuncao().name(),
-                        "formacao", criador.getFormacao() != null ? criador.getFormacao() : "",
-                        "isAdmin", false
-                ));
-            } else {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Consistência de dados comprometida");
-            }
-        } else if (usuario.getFuncao().equals(Funcao.ADMINISTRADOR)) {
-            return ResponseEntity.ok(Map.of(
-                    "id", usuario.getId(),
-                    "nome", usuario.getNome(),
-                    "email", usuario.getEmail(),
-                    "isCriador", false,
-                    "funcao", usuario.getFuncao().name(),
-                    "interesses", usuario.getInteresses() != null ? usuario.getInteresses() : "",
-                    "isAdmin", true,
-                    "fotoUrl", usuario.getFotoUrl() != null ? usuario.getFotoUrl() : ""
-            ));
                 response.put("formacao", criador.getFormacao() != null ? criador.getFormacao() : "");
             }
         } else {
-            return ResponseEntity.ok(Map.of(
-                    "id", usuario.getId(),
-                    "nome", usuario.getNome(),
-                    "email", usuario.getEmail(),
-                    "isCriador", false,
-                    "funcao", usuario.getFuncao().name(),
-                    "interesses", usuario.getInteresses() != null ? usuario.getInteresses() : "",
-                    "isAdmin", false
-            ));
-        }
             response.put("interesses", usuario.getInteresses() != null ? usuario.getInteresses() : "");
             response.put("fotoUrl", usuario.getFotoUrl() != null ? usuario.getFotoUrl() : "");
         }
